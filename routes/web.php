b64d40b8b4b5389341a34c17dad212d4c485d711
AnupShrestha28 <anupstha0168@gmail.com>
2022-12-30 00:14:06 +0545

    Admin Logout function Added

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,9 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth', 'role:admin'])->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+
+    Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('admin.logout');
+
 });
 
 // Vendor Dashboard
@@ -46,6 +49,7 @@ Route::middleware(['auth', 'role:vendor'])->group(function(){
 });
 
 
+Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
 
 
 
