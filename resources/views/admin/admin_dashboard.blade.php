ff97980ef1963ebecc32b898d5e120385e7cfec9
Dhiraj Karki <karkidhiraj56@gmail.com>
2023-01-17 22:01:42 +0545

    Static Data table added for brand

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -24,6 +24,8 @@
 	<link rel="stylesheet" href="{{ asset('adminbackend/assets/css/semi-dark.css')}}" />
 	<link rel="stylesheet" href="{{ asset('adminbackend/assets/css/header-colors.css')}}" />
 
+	<link href="{{ asset('adminbackend/assets/plugins/datatable/css/dataTables.bootstrap5.min.css')}}" rel="stylesheet" />
+
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
 
 	<title>Admin Dashboard</title>
@@ -75,6 +77,13 @@
 		  });
 	  </script>
 	  <script src="{{ asset('adminbackend/assets/js/index.js')}}"></script>
+
+	  <script src="{{ asset('adminbackend/assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
+	  <script>
+		$(document).ready(function() {
+			$('#example').DataTable();
+		  } );
+	</script>
 	<!--app JS-->
 	<script src="{{ asset('adminbackend/assets/js/app.js')}}"></script>
 
