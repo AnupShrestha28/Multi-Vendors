732f82b0ee85dbafc7cf62092be6b39a04d2f398
AnupShrestha28 <anupstha0168@gmail.com>
2023-01-20 21:01:56 +0545

    Admin Category section table added

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\VendorController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Backend\BrandController;
+use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\SocialController;
 use Illuminate\Support\Facades\Auth;
 
@@ -115,6 +116,22 @@ Route::middleware(['auth','role:admin'])->group(function(){
         // Brand All Routes
     Route::controller(BrandController::class)->group(function(){
         Route::get('all/brand', 'AllBrand')->name('all.brand');
+
+        Route::get('add/brand', 'AddBrand')->name('add.brand');
+
+        Route::post('store/brand', 'StoreBrand')->name('store.brand');
+
+        Route::get('edit/brand/{id}', 'EditBrand')->name('edit.brand');
+
+        Route::post('update/brand', 'UpdateBrand')->name('update.brand');
+
+        Route::get('delete/brand/{id}', 'DeleteBrand')->name('delete.brand');
+
+    });
+
+    // Category All Route
+    Route::controller(CategoryController::class)->group(function(){
+        Route::get('all/category', 'AllCategory')->name('all.category');
 
         Route::get('add/brand', 'AddBrand')->name('add.brand');
 
