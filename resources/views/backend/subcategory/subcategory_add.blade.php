ca89b779347f77d49f737fe6ea007d4a8e4bfaa8
AnupShrestha28 <anupstha0168@gmail.com>
2023-01-21 17:33:23 +0545

    admin subcategory info added into db

--- resources/views/backend/subcategory/subcategory_add.blade.php
+++ resources/views/backend/subcategory/subcategory_add.blade.php
@@ -72,7 +72,7 @@
                             <div class="row">
                                 <div class="col-sm-3"></div>
                                 <div class="col-sm-9 text-secondary">
-                                    <input type="submit" class="btn btn-primary px-4" value="Save Changes" />
+                                    <input type="submit" class="btn btn-primary px-4" value="Save" />
                                 </div>
                             </div>
                         </div>
