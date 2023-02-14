542bb4dbbbdd73dfefe1b0e3c756677e20ccf405
AnupShrestha28 <anupstha0168@gmail.com>
2023-02-15 01:50:43 +0545

    User Orders Page BUG Fixed and Orders Details added

--- resources/views/frontend/userdashboard/user_order_page.blade.php
+++ resources/views/frontend/userdashboard/user_order_page.blade.php
@@ -72,7 +72,7 @@
     @endif
                                                         </td>
 
-    <td><a href="{{ url('user/order_details/'.$orders->id)}}" class="btn-sm btn-success"><i class="fa fa-eye"></i> View</a>
+    <td><a href="{{ url('user/order_details/'.$order->id)}}" class="btn-sm btn-success"><i class="fa fa-eye"></i> View</a>
                                                         
                                                             <a href="#" class="btn-sm btn-danger"><i class="fa fa-download"></i> Invoice</a>
                                                         </td>
