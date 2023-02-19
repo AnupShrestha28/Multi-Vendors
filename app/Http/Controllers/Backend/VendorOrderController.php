1a226da60cd46f233e34c23e1c481bc5a740535a
AnupShrestha28 <anupstha0168@gmail.com>
2023-02-19 18:22:22 +0545

    Vendor got notified after order return from user functionality added

--- app/Http/Controllers/Backend/VendorOrderController.php
+++ app/Http/Controllers/Backend/VendorOrderController.php
@@ -18,4 +18,23 @@ class VendorOrderController extends Controller
         $orderitem = OrderItem::with('order')->where('vendor_id',$id)->orderBy('id','DESC')->get();
         return view('vendor.backend.orders.pending_orders',compact('orderitem'));
     } // end method
+
+    public function VendorReturnOrder(){
+        $id = Auth::user()->id;
+        $orderitem = OrderItem::with('order')->where('vendor_id',$id)->orderBy('id','DESC')->get();
+        return view('vendor.backend.orders.return_orders',compact('orderitem'));
+    } // end method
+
+    public function VendorCompleteReturnOrder(){
+        $id = Auth::user()->id;
+        $orderitem = OrderItem::with('order')->where('vendor_id',$id)->orderBy('id','DESC')->get();
+        return view('vendor.backend.orders.complete_return_orders',compact('orderitem'));
+    } // end method
+
+    public function VendorOrderDetails($order_id){
+        $order = Order::with('division','district','state','user')->where('id',$order_id)->first();
+
+        $orderItem = OrderItem::with('product')->where('order_id',$order_id)->orderBy('id','DESC')->get();
+        return view('vendor.backend.orders.vendor_order_details',compact('order','orderItem'));
+    } // end method
 }
